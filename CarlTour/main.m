7451eadc3b46e37edd20f1ad74b7d21558b8887d
Daniel Alabi <alabid@carleton.edu>
2014-04-29 20:55:11 -0500

    Initial Commit

---
+++ CarlTour/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  CarlTour
+//
+//  Created by Daniel Alabi on 4/29/14.
+//  Copyright (c) 2014 CarlTour. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "CTAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([CTAppDelegate class]));
+    }
+}
